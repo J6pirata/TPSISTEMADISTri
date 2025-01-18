8c83d927c0c1f25b2b8bff752699576433ef7f24
J6pirata <74542025+J6pirata@users.noreply.github.com>
2025-01-18 00:13:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SerializaçãoTP;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+Console.WriteLine("Hello, World!");
+Voo3 voo3 = new Voo3(); 
+
+voo3.dateTime1 = DateTime.Now;
+voo3.Origem = "Venezuela";
+voo3.Destino = "Brasil";
+voo3.Compania = "Gol";
+voo3.Operando = true;
+
+FileStream fs = new FileStream("Voo3.bin", FileMode.Create);
+BinaryFormatter bf = new BinaryFormatter();
+bf.Serialize(fs, voo3);
+fs.Close();
+Console.WriteLine("serializado com sucesso");
+
+
+FileInfo fi = new FileInfo("Voo3.bin");
+if (fi.Exists)
+{
+    FileStream fileclose = new FileStream("Voo3.bin", FileMode.Open);
+    BinaryFormatter vooozinho = new BinaryFormatter();
+    Voo3 novo = (Voo3)bf.Deserialize(fileclose);
+    fileclose.Close();
+}
+Console.WriteLine("dados do voo");
+Console.WriteLine($"Origem :{voo3.Origem}");
+Console.WriteLine($"Destino : {voo3.Destino}");
+Console.WriteLine($"Compania : {voo3.Compania}");
+Console.WriteLine($"dateTime1 : `{voo3.dateTime1}");
+Console.ReadKey();
+
+FileStream fileclose2 = new FileStream("voo3.xml", FileMode.Create);
+XmlSerializer xs = new XmlSerializer(typeof(Voo3));
+xs.Serialize(fileclose2, voo3);
+fileclose2.Close();
+Console.WriteLine(xs);
+Console.WriteLine("feitro com sucesso");
+Console.ReadKey();
+
+
---
+++ Voo3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializaçãoTP
+{
+    [Serializable]
+    internal class Voo3
+    {
+
+        private string origem;
+        private string destino;
+        private DateTime DateTime;
+        private string compania;
+        private bool operando;
+
+        public string Origem
+        {
+            set { origem = value; }
+            get { return origem; }
+        }
+        public string Destino
+        { set { destino = value; } get { return destino; } }
+
+        public DateTime dateTime1
+        {
+            set { DateTime = value; }
+            get { return DateTime; }
+        }
+
+        public string Compania
+        {
+            set { compania = value; }
+            get { return compania; }
+        }
+
+        public bool Operando
+        {
+            set => operando = value; get { return operando; }
+        }
+
+        public Voo3() { }
+        public Voo3(string origem, DateTime dateTime, string compania, bool operando)
+        {
+            this.origem = origem;
+            DateTime = dateTime;
+            this.compania = compania;
+            this.operando = operando;
+        }
+
+    }
+}
